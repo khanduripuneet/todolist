f635dde89e83faf91e6121b513a94438c9297ad3
Puneet Khanduri <pkhanduri@twitter.com>
2013-10-15 23:08:51 -0700

    Initial Commit

---
+++ todolist/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todolist
+//
+//  Created by Puneet Khanduri on 10/15/13.
+//  Copyright (c) 2013 Puneet Khanduri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ todolist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  todolist
+//
+//  Created by Puneet Khanduri on 10/15/13.
+//  Copyright (c) 2013 Puneet Khanduri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
